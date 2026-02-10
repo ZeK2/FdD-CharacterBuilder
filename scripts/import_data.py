470f2f6b2bfaa2ac1238b91093ebe92c2f30c701
LeFoll Alexandre <alexandre.le-foll@telecomnancy.eu>
2026-02-10 20:07:14 +0100

    Begin import script Class Tree #2

---
+++ scripts/import_data.py
@@ -0,0 +1,86 @@
+import pdfplumber
+from pathlib import Path
+import pandas as pd
+import unicodedata
+import os
+from neo4j import GraphDatabase
+
+RESSOURCES_FOLDER = "ressources/"
+CLASS_TREE_PDF = "FdD_ACC_*.pdf"
+
+
+
+class Settings:
+    NEO4J_URI = os.getenv("NEO4J_URI", "bolt://0.0.0.0:7687")
+    NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+
+settings = Settings()
+
+driver = GraphDatabase.driver(
+    settings.NEO4J_URI,
+    auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD)
+)
+
+def get_session():
+    return driver.session()
+
+
+base = Path(RESSOURCES_FOLDER)
+
+#Parsing de l'arbre des classes
+def parse_class_tree():
+
+
+    ctfs = [file for file in base.rglob(CLASS_TREE_PDF) if file.is_file()]
+    if len(ctfs) != 1:
+        print("Error cannot find class tree file")
+        exit(1)
+    ctf = ctfs[0]
+
+    class_dict = dict()
+    relation_dict = dict()
+
+    Label = ["BasicClass","AdvancedClass","SupremeClass"]
+
+    def to_id(name):
+        return ''.join(
+            c for c in unicodedata.normalize('NFD', name.lower())
+            if unicodedata.category(c) != 'Mn'
+        ).replace(' ', '').replace("’",'')
+        
+
+    def parse_table_column(serie):
+        trim_serie = serie[serie.notnull()]
+        label_special = Label[trim_serie.name]
+        for classes in trim_serie:
+            for classe in classes.split(' | '):
+                id = to_id(classe)
+                try :
+                    class_dict[id]
+                except:
+                    class_dict[id]=f"({id}:Class:{label_special} {{name:'{classe}'}})"
+
+    def link_table_line(serie,base):
+        previous = base
+        for classes in serie[serie.notnull()]:
+            for classe in classes.split(" | "):
+                clean_classe = to_id(classe)
+                try:
+                    relation_dict[f"{previous}-{clean_classe}"]
+                except:
+                    relation_dict[f"{previous}-{clean_classe}"] = f"({previous})-[:Evolve]->({to_id(classe)})"
+            previous = to_id(classes)
+
+    with pdfplumber.open(ctf) as pdf:
+        for page_id in range(2,len(pdf.pages)):
+            page = pdf.pages[page_id]
+            for table in page.extract_tables():
+                table_pd = pd.DataFrame(table)
+                table_pd.apply(parse_table_column ,axis=0)
+
+                base_class = to_id(table_pd.iloc[0,0])
+                table_pd.iloc[:,1:].apply(lambda x: link_table_line(x,base_class),axis=1)
+
+    query = f"CREATE {','.join(class_dict.values())},{','.join(relation_dict.values())}"
+    get_session().run(query)
